d80befd9291f7dfd40368c8fedf5d16f86dff25d
Chase <chase@cfac-pr-imac-07.byu.edu>
2018-11-13 15:26:14 -0700

    additional stalling changes

--- lib/sheets.py
+++ lib/sheets.py
@@ -1,20 +1,49 @@
 import gspread
-
+from gspread_formatting import *
+import datetime
+import string
+import time
 
 def upload_data_to_sheets(sh, website_data, sh_name):
     ## TODO
     # create header on row 2
     worksheet = sh.open(sh_name).get_worksheet(0)
+    # create header
+    worksheet.update_cell(2, 1, 'Website')
+    worksheet.update_cell(2, 2, 'Metrics')
+    for i, month in enumerate(website_data[0]['data']):
+        month_name = datetime.date.fromisoformat(month['month']).strftime("%B")
+        worksheet.update_cell(2, i + 3, month_name)
     row = 3
+    print('Uploading data to Google Sheets')
     for website in website_data:
-        worksheet.update_cell(row, 1, website['website'])
+        print(website['website'].strip())
+        worksheet.update_cell(row + 1, 1, website['website'].strip())
         col = 3
         worksheet.update_cell(row, 2, 'Monthly Users')
         worksheet.update_cell(row + 1, 2, 'Monthly Pageviews')
         worksheet.update_cell(row + 2, 2, 'Top Pages')
-        for month in website['data']:
+        
+        for i, month in enumerate(website['data']):
             worksheet.update_cell(row, col, month['users'])
+            # APPLY COLOR FORMATTING
+            if i is not 0:
+                good_color = month['users'] > website['data'][i - 1]['users']
+                apply_color_formatting(worksheet, good_color, row, col)
             worksheet.update_cell(row + 1, col, month['unique_pageviews'])
+            if i is not 0:
+                good_color = month['unique_pageviews'] > website['data'][i - 1]['unique_pageviews']
+                apply_color_formatting(worksheet, good_color, row + 1, col)
             worksheet.update_cell(row + 2, col, '\n'.join(month['top_pages']))
             col = col + 1
         row = row + 4
+        time.sleep(100)
+
+def apply_color_formatting(worksheet, good_color, row, col):
+    color = Color(56 / 255, 118 / 255, 29 / 255)
+    if not good_color:
+        color = Color(204 / 255, 0, 0)
+    fmt = CellFormat(textFormat=TextFormat(foregroundColor=color))
+    cell_range = string.ascii_uppercase[col - 1] + str(row)
+    cell_range = cell_range + ':' + cell_range
+    format_cell_range(worksheet, cell_range, fmt)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from lib.analytics import get_leaderboard_data
 from lib.sheets import upload_data_to_sheets
 import httplib2
 import gspread
+import time
 import json
 
 scope = ['https://www.googleapis.com/auth/analytics.readonly',
@@ -12,22 +13,31 @@ scope = ['https://www.googleapis.com/auth/analytics.readonly',
 YEAR = 2018
 MONTH = 1
 MONTHS_TO_RETURN = 12
+VIEW_IDS = 'view_ids.csv'
+CREDENTIALS = 'client_secret.json'
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS, scope)
 http = creds.authorize(httplib2.Http())
 
 sheets_manager = gspread.authorize(creds)
 analytics_service = build('analytics', 'v4', http=http)
 
 website_data = []
-with open('view_ids.csv') as f:
+print('Pulling data from Google Analytics')
+with open(VIEW_IDS) as f:
     for line in f.readlines():
         view_id, website = line.split(',')
+        print(website.strip())
         data = get_leaderboard_data(analytics_service, view_id, YEAR, MONTH, MONTHS_TO_RETURN)
         website_data.append({
             'website': website,
             'data': data
         })
+        # have to sleep due to requests quota per 100 seconds
+        # interestingly enough, batching requests doesn't affect what is counted 
+        # towards quota, as they're interally counted. In other words, there's no way
+        # to get all this data without going over the quota
+        time.sleep(100)
 
 #print(json.dumps(website_data, sort_keys=True, indent=2))
 
